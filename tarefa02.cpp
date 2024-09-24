a011ab7cd7149e48595c153ce189f256d2285390
Jonatan Fragoso <jonatanfragoso@gmail.com>
2024-09-24 09:17:52 -0500

    deletando arquivos duplicados

---
+++ tarefa02.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int age;
+	string name;
+
+	cout << "Escreva seu nome:" << endl;
+	cin >> name;
+	cout << "Escreva sua idade:" << endl;
+	cin >> age;
+
+
+	cout << "Seu nome e :" << name << "e sua idade e :" << age << endl;
+	return 0;
+}
+
+//int main() {
+//    std::string nome;
+//    int numero;
+//
+//    std::cout << "Olá!" << std::endl;
+//
+//    std::cout << "Digite seu nome: ";
+//    std::cin >> nome;
+//
+//    std::cout << "Digite sua idade: ";
+//    std::cin >> numero;
+//
+//    std::cout << "Seu nome é: " << nome << std::endl;
+//    std::cout << "Sua idade é: " << numero << std::endl;
+//    return 0;
+//}
