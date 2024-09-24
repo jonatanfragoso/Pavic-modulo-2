f4c073726f84ea6d009868e9289edd78ae74b4de
Jonatan Fragoso <jonatanfragoso@gmail.com>
2024-09-24 09:35:42 -0500

    script para imprimir o nome e a idade

--- tarefa02.cpp
+++ tarefa02.cpp
@@ -13,23 +13,6 @@ int main()
 	cin >> age;
 
 
-	cout << "Seu nome e :" << name << "e sua idade e :" << age << endl;
+	cout << "Seu nome e : " << name << "e sua idade e : " << age << endl;
 	return 0;
 }
-
-//int main() {
-//    std::string nome;
-//    int numero;
-//
-//    std::cout << "Olá!" << std::endl;
-//
-//    std::cout << "Digite seu nome: ";
-//    std::cin >> nome;
-//
-//    std::cout << "Digite sua idade: ";
-//    std::cin >> numero;
-//
-//    std::cout << "Seu nome é: " << nome << std::endl;
-//    std::cout << "Sua idade é: " << numero << std::endl;
-//    return 0;
-//}
