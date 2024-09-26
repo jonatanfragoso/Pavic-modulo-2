1ecb8ab9843c55a771eb0b7240f69979fbb9fec8
Jonatan Fragoso <jonatanfragoso@gmail.com>
2024-09-25 22:14:44 -0500

    corrigindo repositorio

---
+++ Aula_01/Aula_01_Tarefa_02.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int age;
+	string name;
+
+	cout << "Escreva seu nome:" << endl;
+	cin >> name;
+	cout << "Escreva sua idade:" << endl;
+	cin >> age;
+	cout << "Seu nome e: " << name << " e sua idade e: " << age << endl;
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pavic-modulo-2
+
+Repositório do módulo 2 do treinamento de Visão computacional - PAVIC
